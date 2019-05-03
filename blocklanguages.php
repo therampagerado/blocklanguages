694a5282e3bd17ec0597d19e7f6d75c483385ba6
Markus Hitter <mah@jump-ing.de>
2019-05-04 01:06:08 +0200

    Add constructor entry 'tb_min_version'.
    This is a requirement introduced recently by the validation script.

--- blocklanguages.php
+++ blocklanguages.php
@@ -41,6 +41,7 @@ class BlockLanguages extends Module
 		$this->displayName = $this->l('Block Languages');
 		$this->description = $this->l('Adds a block allowing customers to select a language for your store\'s content.');
 		$this->tb_versions_compliancy = '> 1.0.0';
+		$this->tb_min_version = '1.0.0';
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 	}
 
